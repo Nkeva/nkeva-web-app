1943cb96f4944227929890d30db9a045963764dd
VoDACode <dimavovk201112@gmail.com>
2023-06-15 20:05:19 +0300

    Added HeadTeacherAuthorizeAttribute Removed example code

---
+++ nkeva-web-app/Attributes/HeadTeacherAuthorizeAttribute.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace nkeva_web_app.Attributes
+{
+    public class HeadTeacherAuthorizeAttribute : AuthorizeAttribute
+    {
+        public HeadTeacherAuthorizeAttribute()
+        {
+            Policy = "SchoolRole_HeadTeacher";
+        }
+    }
+}
